7f81e2ad5d52cda93c49ac4a90e7b69758d43b5e
WD Zone <117820644+angel-loveyou@users.noreply.github.com>
2023-01-21 20:09:32 +0530

    op

--- Process/filters.py
+++ Process/filters.py
@@ -1,6 +1,6 @@
 from pyrogram import filters
 from typing import List, Union
-from RaiChu.config import COMMAND_PREFIXES
+from WOODcraft.config import COMMAND_PREFIXES
 
 
 other_filters = filters.group & ~filters.edited & ~filters.via_bot & ~filters.forwarded
